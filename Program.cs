c766bad1a1a7f23d1b682398587904f95e178a49
muhammedkan <muhammedkan@gmail.com>
2020-12-19 21:53:32 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumSwaps2
+{
+    class Program
+    {
+        private static int counter = 0;
+        static int minimumSwaps(int[] arr)
+        {
+            int temp1,temp2;
+            int count = arr.Length;
+            bool isFinished= true;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] != i + 1)
+                {
+                    temp1 = arr[i] - 1;
+                    temp2 = arr[temp1];
+
+                    arr[temp1] = arr[i];
+                    arr[i] = temp2;
+                    counter++;
+                    isFinished = false;
+                }
+            }
+            if (!isFinished)
+            {
+                isFinished = true;
+                minimumSwaps(arr);
+            }
+
+            return counter;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string array = "4 3 1 2";
+            int[] arr = Array.ConvertAll(array.Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int res = minimumSwaps(arr);
+        }
+    }
+}
